d239a4b9397bb2ebea6225e4156bea8045f3aa55
nick adams <thebigdeal916@icloud.com>
2021-08-14 16:29:39 -0500

    added more string methods 8-14

--- js/strings.js
+++ js/strings.js
@@ -1,8 +1,8 @@
 'use strict';
-// --------  STRING MANIPULATION  -------- //
+// --------  STRING METHODS  -------- //
 (function() {
 
-    // function returning a string
+    // function returning the argument
     function readInput(str) {
         return str;
     }
@@ -10,21 +10,36 @@
 
     // function finding a strings length
     function stringLength(str) {
-        return str.length
+        return str.length;
     }
     console.log(stringLength('hello people'));
 
     // reversing a string
     function reverseString(str) {
-        return str. split('').reverse().join('')
+        return str.split('').reverse().join('');
     }
     console.log(reverseString('hello world'));
 
     //indexOf function
     var str = 'Nicholas Adams';
-    console.log(str.indexOf(('N')));
-    //logging the length of variable str
+    console.log(str.indexOf('c'));
+
+    // logging the length of variable str
     console.log(str.length);
-    // remove first character of a string(substring)
-    console.log(str.substr(9));
+
+    // remove first character of a string(substring or substr)
+    console.log(str.substr(7));
+
+    // changing the str variable to all upper case
+    console.log(str.toUpperCase());
+
+    // changing the str variable to all lower case
+    console.log(str.toLowerCase());
+
+    // replacing parts of our string using the replace method
+    console.log(str.replace('Nicholas', 'Nick'));
+
+
+
+
 })()
