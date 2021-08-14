e8d3aef99a943a745d63ad9095eae314e69ac957
nick adams <thebigdeal916@icloud.com>
2021-08-14 10:00:15 -0500

    basic math function practice 8-14

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-alert('i work')
+'use strict';
+// IIFE function that will wrap all our js
+(function() {
+    // finding the length of a string //
+    function  stringLength(str) {
+        for(var i = 0; i < str.length; i++) {
+            return str.length;
+        }
+    }
+    console.log(stringLength('hello cruel worl'));
+
+    // reversing a string //
+    function revString(str) {
+        return str.split('').reverse().join('');
+    }
+    console.log(revString('hello'));
+
+    // basic math //
+
+    // Addition
+    function add(a, b) {
+        return a + b;
+    }
+    console.log(add(2, 4));
+
+    // subtraction
+    function subtract(a, b) {
+        return a - b;
+    }
+    console.log(subtract(6, 3));
+
+    // Multiply
+    function multiply(a, b) {
+        return a * b;
+    }
+    console.log(multiply(3, 6));
+
+    // Divide
+    function divide(a, b) {
+        return a / b;
+    }
+
+    console.log(divide(9, 3));
+})()
