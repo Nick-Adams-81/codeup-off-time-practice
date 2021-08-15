cb31e1fff789da91b0a7cc321741df02993473b1
nick adams <thebigdeal916@icloud.com>
2021-08-15 08:35:21 -0500

    feat: added more user questions using prompt and alert

--- js/prompt.js
+++ js/prompt.js
@@ -12,5 +12,11 @@
     var sport = prompt('What is your favorite sport ' + name + '?');
     alert('I love ' + sport + ' too ' + name + '!!!');
 
+    alert('So ' + name + ', your favorite food is ' + food + ', and your favorite sport is ' + sport + '.');
+
+    var lastQ = prompt('Last question ' + name + ', who has the best coding bootcamp ever?');
+    alert('That\'s right ' + name + '! ' + lastQ + ' has the best coding bootcamp ever!!!');
+    alert('Well ' + name + ', it was nice to meet you! We should go to a ' + sport + ' game soon and get some ' + food + ' when were there, ' +
+        'when you have free time from ' + lastQ + ' that is!')
 
 })()
