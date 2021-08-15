27cc9d2b234e364dbefa783c816f3573fdc9ccb2
nick adams <thebigdeal916@icloud.com>
2021-08-15 08:25:02 -0500

    added favorite food prompt

--- js/prompt.js
+++ js/prompt.js
@@ -4,4 +4,8 @@
     // prompt asking a users name
     var name = prompt('Whats is your name?');
     alert('Nice to meet you ' + name);
+
+    // prompt asking a users favorite food
+    var food = prompt('What is your favorite food ' + name + '?');
+    alert(food + ' is my favorite too ' + name + '!');
 })()
