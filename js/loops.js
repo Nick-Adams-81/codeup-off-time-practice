adc0e07db5e7ff9c7cf236bb1df3a23f3f9229f3
nick adams <thebigdeal916@icloud.com>
2021-08-21 07:29:40 -0500

    for loop counting down added

--- js/loops.js
+++ js/loops.js
@@ -6,4 +6,11 @@
     // for loop counting up
     for(var i = 0; i < 10; i++)
         console.log(i);
+
+    // for loop counting down
+    for(var i = 10; i > 0; i--) {
+        console.log(i);
+    }
+
+
 })()
