12dc0de6010e5da8dc7ec54275bb773f15c6378c
nick adams <thebigdeal916@icloud.com>
2021-08-21 07:42:02 -0500

    added unshift function to add item to the start of an array

--- js/arrays.js
+++ js/arrays.js
@@ -10,5 +10,9 @@
     // finding the index of one item in the array
     console.log(names.indexOf('Jenny'));
 
+    // inserting an item to the front of the array
+
+    names.unshift('T-1000');
+    console.log(names);
 
 })()
