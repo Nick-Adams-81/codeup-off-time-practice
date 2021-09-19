c4692db2518722a8ece75aee963f9dc7138c8d05
nick adams <thebigdeal916@icloud.com>
2021-09-19 13:00:15 -0500

    twoSum added

--- js/fizz-buzz.js
+++ js/fizz-buzz.js
@@ -1,6 +1,6 @@
-(function() {
+(() => {
 
-
+// fizz buzz
     for(let i = 1; i < 100; i++) {
         if(i % 15 === 0) {
             console.log('fizz buzz');
@@ -12,4 +12,16 @@
             console.log(i)
         }
     }
+
+        // some es-6 stuff
+    const twoSum = (input1, input2, target) => {
+        if(input1 + input2 === target) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    console.log(twoSum(5, 6, 11));
+
 })()
