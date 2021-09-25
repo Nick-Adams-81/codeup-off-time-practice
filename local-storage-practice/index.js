0a91de2384cc86a10f2eb6d1eb3a4273bbd3883a
nick adams <thebigdeal916@icloud.com>
2021-09-25 16:48:14 -0500

    used an array for key

--- local-storage-practice/index.js
+++ local-storage-practice/index.js
@@ -2,10 +2,8 @@ console.log('hello')
 
 
 function save() {
-
     var text = document.getElementById('text').value
     var content = document.getElementById('content')
     localStorage.setItem('input', text)
-    var newText = localStorage.getItem('input')
-    content.innerHTML = newText;
+    content.innerHTML = localStorage['input'];
 }
