183c4c0642a7db1ffcbd39ea5b73e85d035d1810
nick adams <thebigdeal916@icloud.com>
2021-09-25 14:04:14 -0500

    added getItem

--- local-storage-practice/index.js
+++ local-storage-practice/index.js
@@ -2,8 +2,10 @@ console.log('hello')
 
 
 function save() {
+
     var text = document.getElementById('text').value
     var content = document.getElementById('content')
     localStorage.setItem('input', text)
-
+    var newText = localStorage.getItem('input')
+    content.innerHTML = newText;
 }
