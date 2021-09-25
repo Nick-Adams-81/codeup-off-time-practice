4533977b0b9784ab17fc6d845c0cbcc3a796ad7f
nick adams <thebigdeal916@icloud.com>
2021-09-25 13:51:35 -0500

    added local storage set item

--- local-storage-practice/index.js
+++ local-storage-practice/index.js
@@ -1,1 +1,9 @@
 console.log('hello')
+
+
+function save() {
+    var text = document.getElementById('text').value
+    var content = document.getElementById('content')
+    localStorage.setItem('input', text)
+
+}
