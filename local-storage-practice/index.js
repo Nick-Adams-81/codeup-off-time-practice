e7f5561284c08fd7b893ed1a69086def97cec359
nick adams <thebigdeal916@icloud.com>
2021-09-26 12:34:48 -0500

    discovered data persistent issue

--- local-storage-practice/index.js
+++ local-storage-practice/index.js
@@ -1,9 +1,9 @@
 console.log('hello')
 
 
-function save() {
-    var text = document.getElementById('text').value
-    var content = document.getElementById('content')
-    localStorage.setItem('input', text)
-    content.innerHTML = localStorage['input'];
-}
+var text = document.getElementById('text').value
+var content = document.getElementById('content')
+localStorage.setItem('input', text)
+var data = localStorage.getItem('input')
+content.innerHTML = data
+
